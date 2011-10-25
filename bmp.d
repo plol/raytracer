5a99893a38b61a207ce24cb8970949c7739bd4c4
plol <pelle.mansson@gmail.com>
2011-10-25 18:32:56 +0200

    fixed bmp

--- bmp.d
+++ bmp.d
@@ -19,25 +19,39 @@ ubyte color_value(double x) {
 }
 
 ubyte[] encode(Color[][] colors) {
+
+    uint width = colors[0].length;
+    uint height = colors.length;
+
     ubyte[] bmp = new ubyte[](0x36);
     bmp[0x00 .. 0x02] = [0x42, 0x4D]; //B M
     bmp[0x0a] = 0x36; //offset
     bmp[0x0e] = 0x28; // dib header size ???
-    bmp[0x12 .. 0x16] = get_ubytes(colors[0].length);    // image size
-    bmp[0x16 .. 0x1a] = get_ubytes(colors.length); // image size
+    bmp[0x12 .. 0x16] = get_ubytes(width);    // image size
+    bmp[0x16 .. 0x1a] = get_ubytes(height); // image size
     bmp[0x1a] = 0x01; // 1 plane
     bmp[0x1c] = 0x18; // 24 bits
     bmp[0x26 .. 0x2a] = get_ubytes(2835u); // pixels per meter, lol
     bmp[0x2a .. 0x2e] = get_ubytes(2835u); // pixels per meter, lol
     // 0x2e .. 0x36 is zeroes
 
+    size_t row_length = width * 3;
+
+    while (row_length % 4) {
+        row_length += 1;
+    }
+
+    ubyte[] row_data = new ubyte[](row_length);
+
     foreach (row; retro(colors)) {
+        size_t i = 0;
         foreach (c; row) {
-            bmp ~= color_value(c.z);
-            bmp ~= color_value(c.y);
-            bmp ~= color_value(c.x);
+            row_data[i++] = color_value(c.b);
+            row_data[i++] = color_value(c.g);
+            row_data[i++] = color_value(c.r);
         }
-        bmp.length += (bmp.length + 2) % 4;
+        bmp ~= row_data;
+        row_data[] = 0;
     }
 
     bmp[0x02 .. 0x06] = get_ubytes(bmp.length); // total bmp size;
